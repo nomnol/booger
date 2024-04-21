24a4931b800f6d7b030bae8bfe471803f5e6bf73
keekyPo <goldenveizer@outlook.com>
2024-04-21 04:17:11 +0300

    burned updated

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ const onCopyClick = async (address) => {
           </li>
           <li class="booger__data-box-item booger__data-box-item_extra-column">
             <dfn class="booger__data-header">Burned:</dfn>
-            <small class="booger__data-value booger__data-value_burned">730m</small>
+            <small class="booger__data-value booger__data-value_burned">850m</small>
             <ins class="booger__data-value_ca-burned">
                 <span class="booger__ca booger__ca_burned">
                   {{CA_BURNED}}
